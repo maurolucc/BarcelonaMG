f3e8fe4741f0b27e51f81aff7505c1cff048701a
maurolucc <maurolucchinidepompa@gmail.com>
2015-02-04 19:51:34 +0100

    just show name list

--- app/src/main/java/com/example/mauro/barcelonamg/MainActivity.java
+++ app/src/main/java/com/example/mauro/barcelonamg/MainActivity.java
@@ -4,16 +4,73 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import org.apache.commons.logging.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView mListView;
+    private ArrayAdapter<String> adapter;
+    private List<ParseObject> onlineData;
+    private ArrayList<String> spots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        spots=new ArrayList<>();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "3PZjEECwDOYgxpqkzRPseHbistE33bEBC0cDl8DC", "m5uh4bkVr9HWkZ5jhK5tfcscBuA7xtyeYCILI1t0");
+
+
+        fetchData("Name");
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             //expand the Item Selected in a second Activity
+            }
+        });
+
+        mListView.setAdapter(adapter);
+
     }
 
+    public void fetchData(String key){
+        ParseQuery<ParseObject> query =  ParseQuery.getQuery("Leisure");
+        try{
+            onlineData= query.find();
+            for(ParseObject object: onlineData){
+                String clau= object.get(key).toString();
+                spots.add(clau);
+            }
+            adapter= new ArrayAdapter<String>(getApplicationContext(),R.layout.custom_item, R.id.text1,spots);
+
+        }catch(ParseException e){
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Error on fetching data",Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
